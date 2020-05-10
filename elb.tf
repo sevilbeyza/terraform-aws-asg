c9e291fc4920f02a15db6a94ca44e80d53a0c841
EC2 Default User <ec2-user@ip-172-31-5-215.us-west-1.compute.internal>
2020-05-10 18:02:36 +0000

    update redme file

--- elb.tf
+++ elb.tf
@@ -37,3 +37,7 @@ resource "aws_autoscaling_attachment" "asg_attachment_bar" {
   autoscaling_group_name = "${aws_autoscaling_group.example.id}"
   elb                    = "${aws_elb.bar.id}"
 }
+
+
+
+
